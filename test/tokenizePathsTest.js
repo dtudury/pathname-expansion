fba3626b0e597c48550a5498c4ba12d7296c1bb0
David Tudury <david.tudury@voxer.com>
2013-09-18 10:36:18 -0700

    handle tokenizing [, ], and ? wildcards

--- test/tokenizePathsTest.js
+++ test/tokenizePathsTest.js
@@ -10,8 +10,13 @@ var _ = require('lodash');
 var expect = require('chai').expect;
 var tokenizePaths = require('../lib/tokenizePaths');
 
+console.log(tokenizePaths(['a/b?b/c']));
+
 describe('tokenizePaths', function () {
     it('should break path into directories', function () {
         expect(tokenizePaths(['a/b/c'])).to.deep.equal([['a', 'b', 'c']]);
+    });
+    it('should turn wildcards into regular expressions', function () {
+        expect(tokenizePaths(['a/b?b/c'])).to.deep.equal([['a', /^b.b$/, 'c']]);
     });
 });
